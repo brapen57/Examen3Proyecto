fca69342d1d8dfb99d9a491b50fe6b8d77af01a2
brapen57 <144976865+brapen57@users.noreply.github.com>
2025-01-23 09:04:59 -0300

    Add files via upload

---
+++ build/precache-manifest.de2fc19461a27c6ec7e3ecc3c0e89162.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ca115ce029a190950c0b3ff392b3ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ef8a17958ce7918703d",
+    "url": "./static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "5862074c1ec8889562b0",
+    "url": "./static/css/main.e2de2162.chunk.css"
+  },
+  {
+    "revision": "5ef8a17958ce7918703d",
+    "url": "./static/js/2.5e9febad.chunk.js"
+  },
+  {
+    "revision": "b14cf38e36b679bf3c7c",
+    "url": "./static/js/3.bb0829b5.chunk.js"
+  },
+  {
+    "revision": "5862074c1ec8889562b0",
+    "url": "./static/js/main.1368aaa5.chunk.js"
+  },
+  {
+    "revision": "364786473a357f26b98e",
+    "url": "./static/js/runtime~main.4d1b07ba.js"
+  }
+]);
